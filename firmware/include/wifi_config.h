eaac06a6ccfdca8af21af9c150db4fff21107e68
Curtis Gribben <Curtis.Gribben@chubb.com>
2026-08-14 19:45:00 +0100

    Add wifi_config, model headers, and OTA auth flag

---
+++ firmware/include/wifi_config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define WIFI_SSID       "dan_loves_nature"
+#define WIFI_PASSWORD   "Appletree83"
+#define SERVER_IP       "192.168.0.239"
+#define SERVER_PORT     5000
+#define DEVICE_ID       "care-home-1"
+#define HEARTBEAT_MS    10000UL
